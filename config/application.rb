68eaacdcbb3b8cb860e3c3d277ccc4c9b992ac55
FUNABARA Masao <masao@masoo.jp>
2021-06-03 14:18:07 +0000

    add cloudflare proxied mode.

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Nnlh
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.use CloudflareProxy
   end
 end
